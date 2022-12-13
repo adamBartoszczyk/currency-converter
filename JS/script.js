ac8aa43ad41f74002b622da56a8a9d24d2baf92c
Adam Bartoszczyk <adam1bartoszczyk@gmail.com>
2022-12-13 23:33:25 +0100

    functions changes

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,51 @@
-let howMuch = document.querySelector(".js-howMuch");
-let total = document.querySelector(".js-total");
-let currencyForm = document.querySelector(".js-currencyForm");
-let currencyTo = document.querySelector(".js-currencyTo");
-let form = document.querySelector(".js-form");
+{
 
-let PLN = 1.00;
-let USD = 4.77;
-let EUR = 4.72;
+    const howMuchElement = document.querySelector(".js-howMuch");
+    const totalElement = document.querySelector(".js-total");
+    const currencyFormElement = document.querySelector(".js-currencyForm");
+    const currencyToElement = document.querySelector(".js-currencyTo");
+    const formElement = document.querySelector(".js-form");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let currencyFormChoose;
-    let currencyToChoose;
-    let howMuchElement = +howMuch.value;
-    let result;
 
-    switch (currencyForm.value) {
-        case "pln":
-            currencyFormChoose = PLN;
-            break;
-        case "usd":
-            currencyFormChoose = USD;
-            break;
-        case "eur":
-            currencyFormChoose = EUR;
-            break;
+
+
+
+    const chooseCurrency = (currency) => {
+        const PLN = 1.00;
+        const USD = 4.77;
+        const EUR = 4.72;
+
+        switch (currency) {
+            case "pln":
+                return PLN;
+            case "usd":
+                return USD;
+            case "eur":
+                return EUR;
+        };
     };
 
-    switch (currencyTo.value) {
-        case "pln":
-            currencyToChoose = PLN;
-            break;
-        case "usd":
-            currencyToChoose = USD;
-            break;
-        case "eur":
-            currencyToChoose = EUR;
-            break;
-    };
-    
-    result = (howMuchElement * currencyFormChoose) / currencyToChoose;
-    total.value = result.toFixed(2);
-    howMuch.focus();
-});
+    const changeCurrencyToNumber = () => {
+
+        const howMuch = +howMuchElement.value;
+        const currencyForm = currencyFormElement.value;
+        const currencyTo = currencyToElement.value;
+        const currencyFormChoose = chooseCurrency(currencyForm);
+        const currencyToChoose = chooseCurrency(currencyTo);
+
+        const result = (howMuch * currencyFormChoose) / currencyToChoose;
+        totalElement.value = result.toFixed(2);
+    }
+
+
+
+
+    const init = () => {
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            changeCurrencyToNumber();
+        });
+    }
+    init()
+}
